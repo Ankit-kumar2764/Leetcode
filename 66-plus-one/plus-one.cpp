9b80a50a625567dbc1917e7fa01b9be724c43676
Ankit kumar <ay9335144776@gmail.com>
2026-08-05 09:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (95.90%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry=1;
+        int sum=0;
+        int n=digits.size();
+       for(int i=n-1;i>=0;i--){
+           
+           sum=digits[i]+carry;
+           digits[i]=sum%10;
+           carry=sum/10;
+              
+       }
+       if(carry==1)
+      digits.insert(digits.begin(),1);
+          return digits;
+
+        
+    }
+};
