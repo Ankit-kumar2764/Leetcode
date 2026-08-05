1f9f6cde4f7d6d9c2a2d225f3984584c24a1ac78
Ankit kumar <ay9335144776@gmail.com>
2026-08-05 11:31:56 +0530

    Time: 1 ms (4.22%) | Memory: 26.2 MB (36.04%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -11,10 +11,44 @@ public:
     //   return 0; 
 
 
-       int ans=0;
-       for(int i=0;i<nums.size();i++){
-        ans=ans^nums[i];
-       } 
-       return ans;
+
+
+
+    //    int ans=0;
+    //    for(int i=0;i<nums.size();i++){
+    //     ans=ans^nums[i];
+    //    } 
+    //    return ans;
+
+
+
+         int n=nums.size();
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+
+        int st=0,end=n-1;
+       
+        while(st<=end){
+             int mid=st+(end-st)/2;
+        if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) {
+        return nums[mid];
+        }
+        else if(nums[mid]==nums[mid-1]){
+                   if(mid%2==0) end=mid-1;
+                   else  st=mid+1;
+                  }
+
+           else {
+                    
+                    if(mid%2!=0)  end=mid-1;
+                    else    st=mid+1;
+
+           }       
+
+         
+        }
+        return -1;
     }
 };
