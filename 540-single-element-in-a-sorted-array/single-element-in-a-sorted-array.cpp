0bbd799a58960a0936ccc90c1f25b5ec1578e021
Ankit kumar <ay9335144776@gmail.com>
2026-08-05 09:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (94.29%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int ,int>map;
-        for(int x:nums){
-            map[x]++;
-        }
-        for(auto x:map){
-            if(x.second==1) return x.first;
-        }
-      return 0;  
+    //     unordered_map<int ,int>map;
+    //     for(int x:nums){
+    //         map[x]++;
+    //     }
+    //     for(auto x:map){
+    //         if(x.second==1) return x.first;
+    //     }
+    //   return 0; 
+
+
+       int ans=0;
+       for(int i=0;i<nums.size();i++){
+        ans=ans^nums[i];
+       } 
+       return ans;
     }
 };
