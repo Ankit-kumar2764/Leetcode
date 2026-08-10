d4e6d1db07f16014b969dc800d2eb16dac0d7307
Ankit kumar <ay9335144776@gmail.com>
2026-08-10 17:20:27 +0530

    Time: 23 ms (89.10%) | Memory: 10.9 MB (66.57%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<int>t(n+1,false);
+        for(int i=1;i<n+1;i++){
+            for(int k=1;k*k<=i;k++){
+                if(t[i-(k*k)]==false){
+                    t[i]=true;
+                break;
+                }
+            }
+        }
+        return t[n];
+    }
+};
