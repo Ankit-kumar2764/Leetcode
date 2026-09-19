2967d458d133bd3ffbba5aa5d68a2fec8afd55ba
Ankit kumar <ay9335144776@gmail.com>
2026-09-19 20:01:41 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (48.92%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int minOperations(int n) {
-        vector<int>v;
-        for(int i=0;i<n;i++){
-            v.push_back(2*i+1);
+        // vector<int>v;
+        // for(int i=0;i<n;i++){
+        //     v.push_back(2*i+1);
 
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            count+=abs(v[i]-v[n/2]);
+        // }
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     count+=abs(v[i]-v[n/2]);
 
-        }
-        return count/2;
+        // }
+        // return count/2;
+
+        return n*n/4;
     }
 };
